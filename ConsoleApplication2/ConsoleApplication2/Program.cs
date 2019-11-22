eb3e2f9d5c454f0b06e880bc5fdf75b17fb85a89
Josh-Kao-1992 <58066628+Josh-Kao-1992@users.noreply.github.com>
2019-11-22 16:40:43 +0800

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApplication2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            decimal ii;
+//            int x = 5005;
+//            string s1 = "ani\u00ADmal";
+//            string s2 = "animal";
+//            string qq = string.Format("XXX " +Convert.ToDecimal( x) / 1000);
+//            DateTime tnow = DateTime.Now;
+
+//            for (int i = 0; i < 20; i++)
+//            {
+//                DateTime now = tnow.AddDays(-12 - i);
+//                int iTNOW = now.Year * 10000 + now.Month * 100 + now.Day;
+//                string sTNOW = string.Format("{0:00000000}", iTNOW);
+
+//                int qty = -int.Parse("00001000");
+//                Int64 amt = (Int64)(qty * 54);
+
+//                // Find the index of the soft hyphen.
+//                Console.WriteLine(now);
+                
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
